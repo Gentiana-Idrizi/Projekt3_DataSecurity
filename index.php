0ba4fc76f2e0d872141d5dd4f922cff1ad24d46f
florindahasanii <93677978+florindahasanii@users.noreply.github.com>
2022-05-15 13:35:25 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,115 @@
+<?php  
+ $connect = mysqli_connect("localhost", "root", "", "test");  
+ session_start();  
+ if(isset($_SESSION["username"]))  
+ {  
+      header("location:entry.php");  
+ }  
+ if(isset($_POST["register"]))  
+ {  
+      if(empty($_POST["username"]) || empty($_POST["password"]))  
+      {  
+           echo '<script>alert("Both Fields are required")</script>';  
+      }  
+      else  
+      {  
+           $username = mysqli_real_escape_string($connect, $_POST["username"]);  
+           $password = mysqli_real_escape_string($connect, $_POST["password"]);  
+           $password = password_hash($password, PASSWORD_DEFAULT);  
+           $query = "INSERT INTO users(username, password) VALUES('$username', '$password')";  
+           if(mysqli_query($connect, $query))  
+           {  
+                echo '<script>alert("Registration Done")</script>';  
+           }  
+      }  
+ }  
+ if(isset($_POST["login"]))  
+ {  
+      if(empty($_POST["username"]) || empty($_POST["password"]))  
+      {  
+           echo '<script>alert("Both Fields are required")</script>';  
+      }  
+      else  
+      {  
+           $username = mysqli_real_escape_string($connect, $_POST["username"]);  
+           $password = mysqli_real_escape_string($connect, $_POST["password"]);  
+           $query = "SELECT * FROM users WHERE username = '$username'";  
+           $result = mysqli_query($connect, $query);  
+           if(mysqli_num_rows($result) > 0)  
+           {  
+                while($row = mysqli_fetch_array($result))  
+                {  
+                     if(password_verify($password, $row["password"]))  
+                     {  
+                          //return true;  
+                          $_SESSION["username"] = $username;  
+                          header("location:entry.php");  
+                     }  
+                     else  
+                     {  
+                          //return false;  
+                          echo '<script>alert("Wrong User Details")</script>';  
+                     }  
+                }  
+           }  
+           else  
+           {  
+                echo '<script>alert("Wrong User Details")</script>';  
+           }  
+      }  
+ }  
+ ?>  
+ <!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>PHP Login Registration - password_hash()</title>  
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+      </head>  
+      <body>  
+           <br /><br />  
+           <div class="container" style="width:500px;">  
+                <h3 align="center">PHP Login Registration - password_hash() </h3>  
+                <br />  
+                <?php  
+                if(isset($_GET["action"]) == "login")  
+                {  
+                ?>  
+                <h3 align="center">Login</h3>  
+                <br />  
+                <form method="post">  
+                     <label>Enter Username</label>  
+                     <input type="text" name="username" class="form-control" />  
+                     <br />  
+                     <label>Enter Password</label>  
+                     <input type="password" name="password" class="form-control" />  
+                     <br />  
+                     <input type="submit" name="login" value="Login" class="btn btn-info" />  
+                     <br />  
+                     <p align="center"><a href="index.php">Register</a></p>  
+                </form>  
+                <?php       
+                }  
+                else  
+                {  
+                ?>  
+                <h3 align="center">Register</h3>  
+                <br />  
+                <form method="post">  
+                     <label>Enter Username</label>  
+                     <input type="text" name="username" class="form-control" />  
+                     <br />  
+                     <label>Enter Password</label>  
+                     <input type="password" name="password" class="form-control" />  
+                     <br />  
+                     <input type="submit" name="register" value="Register" class="btn btn-info" />  
+                     <br />  
+                     <p align="center"><a href="index.php?action=login">Login</a></p>  
+                </form>  
+                <?php  
+                }  
+                ?>  
+           </div>  
+      </body>  
+ </html>  
